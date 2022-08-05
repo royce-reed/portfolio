6bb8993e0c9b1c220ab085b6dc6de47bb5939636
royce-reed <rjreed1@gmail.com>
2022-08-05 17:46:27 -0500

    added react router and basic nav

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Blogs from './Pages/Blogs';
+import Projects from './Pages/Projects';
+import Contact from './Pages/Contact';
+import Error from './Pages/Error';
 
 const App = () => { 
 
-
   return (
-    <h1>Hello World</h1> 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/blogs" element={<Blogs />} /> 
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 };
 
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
+import NavBar from './Components/NavBar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const container = document.getElementById('root');
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <StrictMode>
+    <Router>
+      <NavBar />
+      <App />
+    </Router>
+  </StrictMode>
+);
